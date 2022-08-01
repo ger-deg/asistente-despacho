94560a6ffc9c1cb9507d7f51910170eb32547cce
ger-deg <deganogerman@gmail.com>
2022-08-01 12:16:44 -0300

    nuevaModicacionConCardDeReguladorYNuevaTabla

--- JS/regulacionmediador.js
+++ JS/regulacionmediador.js
@@ -1,31 +1,31 @@
 let nombreMediador = prompt("Ingresar el nombre del mediador/a");
 let monto = parseInt(prompt("Ingresar el monto del proceso"));
-let valorUhom = 1550;
+let valorUhom = 1710;
 let honorarios;
 function valorCambiante (valor){
   honorarios = valor * valorUhom;
 }
 
-if(monto >= 1 && monto <= 46500) {
+if(monto >= 1 && monto <= 51300) {
     valorCambiante(3);
-} else if (monto > 46500 && monto <= 93000) {
+} else if (monto > 51300 && monto <= 102600) {
     valorCambiante(6);
-} else if (monto > 93000 && monto <= 232500) {
+} else if (monto > 102600 && monto <= 256500) {
     valorCambiante(9);
-} else if (monto > 232500 && monto <= 465000) {
+} else if (monto > 256500 && monto <= 513000) {
     valorCambiante(12);
-} else if (monto > 465000 && monto <= 930000) {
+} else if (monto > 513000 && monto <= 1026000) {
     valorCambiante(16);
-} else if (monto > 930000 && monto <= 1550000) {
+} else if (monto > 1026000 && monto <= 1710000) {
     valorCambiante(20);
-} else {(monto > 1550000) 
+} else {(monto > 1710000) 
     honorarios = parseInt(monto * 2 / 100);
-    if (honorarios > 185999){
-    honorarios = 186000}
+    if (honorarios > 205199){
+    honorarios = 205200}
 };
 
 
 let cantidadDeUhom = honorarios / valorUhom;
-let proveidoRegulacionMediador = `      AUTOS, VISTOS Y CONSIDERANDO:\n     En atención a lo solicitado y al valor actual de la unidad de honorarios de mediación (UHOM), le corresponde a ${nombreMediador} por sus tareas en esa etapa la suma de $${honorarios}, equivalentes a ${cantidadDeUhom} UHOM.\n        Fijase el plazo para su pago en diez días y notifíquese en el domicilio electrónico.`
+let proveidoRegulacionMediador = `      AUTOS, VISTOS Y CONSIDERANDO:<br/>      En atención a lo solicitado y al valor actual de la unidad de honorarios de mediación (UHOM), le corresponde a ${nombreMediador} por sus tareas en esa etapa la suma de $${honorarios}, equivalentes a ${cantidadDeUhom} UHOM.<br/>     Fijase el plazo para su pago en diez días y notifíquese en el domicilio electrónico.`
 
 document.write (proveidoRegulacionMediador);
