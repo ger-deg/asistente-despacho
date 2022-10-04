44522327b31d87f0c867a710356db66f251ca9b7
ger-deg <deganogerman@gmail.com>
2022-10-03 22:31:18 -0300

    haciendoUnGitPullYModificandoElUhom

--- JS/regulacionmediador.js
+++ JS/regulacionmediador.js
@@ -3,7 +3,7 @@ const monto = document.getElementById("monto");
 const btnCalcular = document.getElementById("calcular");
 const resultado = document.getElementById("resultado");
 
-const valorUhom = 1710;
+const valorUhom = 1870;
 let honorarios;
 
 function valorCambiante (valor){
@@ -13,22 +13,22 @@ function valorCambiante (valor){
 btnCalcular.addEventListener("click", (e) => {
     e.preventDefault();
 
-    if(monto.value >= 1 && monto.value <= 51300) {
+    if(monto.value >= 1 && monto.value <= 56100) {
         valorCambiante(3);
-    } else if (monto.value > 51300 && monto.value <= 102600) {
+    } else if (monto.value > 56100 && monto.value <= 112200) {
         valorCambiante(6);
-    } else if (monto.value > 102600 && monto.value <= 256500) {
+    } else if (monto.value > 112200 && monto.value <= 280500) {
         valorCambiante(9);
-    } else if (monto.value > 256500 && monto.value <= 513000) {
+    } else if (monto.value > 280500 && monto.value <= 561000) {
         valorCambiante(12);
-    } else if (monto.value > 513000 && monto.value <= 1026000) {
+    } else if (monto.value > 561000 && monto.value <= 1122000) {
         valorCambiante(16);
-    } else if (monto.value > 1026000 && monto.value <= 1710000) {
+    } else if (monto.value > 1122000 && monto.value <= 1870000) {
         valorCambiante(20);
-    } else {(monto.value > 1710000) 
+    } else {(monto.value > 1870000) 
         honorarios = parseInt(monto.value * 2 / 100);
-        if (honorarios > 205199){
-        honorarios = 205200}
+        if (honorarios > 224399){
+        honorarios = 224400}
     };
     let cantidadDeUhom = honorarios / valorUhom;
     resultado.innerHTML= `<p><strong> AUTOS, VISTOS Y CONSIDERANDO: </strong></p> <p>En atención a lo solicitado y al valor actual de la unidad de honorarios de mediación (UHOM), le corresponde a ${mediador.value} por sus tareas en esa etapa la suma de $${honorarios}, equivalentes a ${cantidadDeUhom} UHOM.<br/>     Fijase el plazo para su pago en diez días y notifíquese en el domicilio electrónico.</p> `
